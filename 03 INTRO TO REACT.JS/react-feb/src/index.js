81bf59ce39a61ae862470e17f302dd8afec9b4cb
TsvetanNikolov123 <tsdman1985@gmail.com>
2020-04-18 17:01:57 +0300

    some tests with ReactJS

--- 03 INTRO TO REACT.JS/react-feb/src/index.js
+++ 03 INTRO TO REACT.JS/react-feb/src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function renderApp() {
+    ReactDOM.render(
+        <Header/>,
+        document.getElementById('root')
+    );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let logoText = 'Logo Placeholder';
+
+const handleNavLink = (event) => {
+    event.preventDefault();
+    const content = event.target.innerHTML;
+    logoText = content;
+    renderApp();
+};
+
+const Logo = () => {
+    return (
+        <div>
+            <h1>{logoText}</h1>
+        </div>
+    );
+};
+
+const Navigation = () => {
+    const className = 'site-nav';
+    const linkClassName = `${className}-link`;
+
+    return (
+        <nav className={className}>
+            <ul>
+                <li><a className={linkClassName} href="/" onClick={handleNavLink}>Home</a></li>
+                <li><a className={linkClassName} href="/contact" onClick={handleNavLink}>Contact</a></li>
+                <li><a className={linkClassName} href="/about" onClick={handleNavLink}>About</a></li>
+            </ul>
+        </nav>
+    )
+};
+
+const Header = () => {
+    return (
+        <header>
+            <Logo/>
+            <Navigation/>
+        </header>
+    );
+};
+
+renderApp();
